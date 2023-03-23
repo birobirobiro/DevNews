056ad7a44ea50053f34227e6d425ae1b0cd40a82
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-03-23 18:10:50 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,33 +1,87 @@
 function GithubTrending() {
-  fetch('https://api.hackertab.dev/data/v2/github/global/daily.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  fetch("https://api.hackertab.dev/data/v2/github/global/daily.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const githubSection = document.querySelector("#github .scroll");
+      const content = data
+        .map(
+          (item) => `
+    <div class="row-news">
+      <a href="${item.url}" target="_blank">
+        <p>${item.title}</p>
+      </a>
+    </div>
+    `
+        )
+        .join("");
+      githubSection.innerHTML = content;
+    })
+    .catch((error) => console.error(error));
 }
 
 function ProductHunt() {
-  fetch('https://api.hackertab.dev/data/v2/producthunt.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  fetch("https://api.hackertab.dev/data/v2/producthunt.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const githubSection = document.querySelector("#product-hunt .scroll");
+      const content = data
+        .map(
+          (item) => `
+    <div class="row-news">
+      <a href="${item.url}" target="_blank">
+        <p>${item.title}</p>
+      </a>
+    </div>
+    `
+        )
+        .join("");
+      githubSection.innerHTML = content;
+    })
+    .catch((error) => console.error(error));
 }
 
 function HackerNews() {
-  fetch('https://api.hackertab.dev/data/v2/hackernews.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  fetch("https://api.hackertab.dev/data/v2/hackernews.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const githubSection = document.querySelector("#hackernews .scroll");
+      const content = data
+        .map(
+          (item) => `
+    <div class="row-news">
+      <a href="${item.url}" target="_blank">
+        <p>${item.title}</p>
+      </a>
+    </div>
+    `
+        )
+        .join("");
+      githubSection.innerHTML = content;
+    })
+    .catch((error) => console.error(error));
 }
 
 function TabNews() {
-  fetch('https://www.tabnews.com.br/api/v1/contents?page=1&per_page=50&strategy=relevant')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  fetch(
+    "https://www.tabnews.com.br/api/v1/contents?page=1&per_page=50&strategy=relevant"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const githubSection = document.querySelector("#tabnews .scroll");
+      const content = data
+        .map(
+          (item) => `
+  <div class="row-news">
+    <a href="${item.url}" target="_blank">
+      <p>${item.title}</p>
+    </a>
+  </div>
+  `
+        )
+        .join("");
+      githubSection.innerHTML = content;
+    })
+    .catch((error) => console.error(error));
 }
 
 GithubTrending();
---
+++ js/weather.js
@@ -0,0 +1,62 @@
+const greeting = document.getElementById("greeting");
+const data = document.getElementById("data");
+const hora = document.getElementById("hora");
+const previsaoTempo = document.getElementById("previsao-tempo");
+
+const agora = new Date();
+const horaAtual = agora.getHours();
+
+window.onload = function () {
+  // Define a saudação com base na hora atual
+  if (horaAtual >= 5 && horaAtual < 12) {
+    greeting.textContent = "Boa tarde!";
+  } else if (horaAtual >= 12 && horaAtual < 18) {
+    greeting.textContent = "Boa tarde!";
+  } else {
+    greeting.textContent = "Boa tarde!";
+  }
+
+  // Exibe a data e hora atual
+  data.textContent = agora.toLocaleDateString("pt-BR", {
+    weekday: "long",
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+  });
+  hora.textContent = agora.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  // Obtém a localização do usuário
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // Obtém a previsão do tempo da região atual usando a API OpenWeatherMap
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=19d46d1f32ff78d3d8ac964b5063827e&lang=pt_br&units=metric`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            const descricao = data.weather[0].description;
+            const temperatura = data.main.temp;
+            previsaoTempo.textContent = `Previsão do tempo: ${descricao} e temperatura de ${temperatura}°C graus.`;
+          })
+          .catch((error) => {
+            console.error(error);
+            previsaoTempo.textContent =
+              "Não foi possível obter a previsão do tempo";
+          });
+      },
+      (error) => {
+        console.error(error);
+        previsaoTempo.textContent = "Não foi possível obter a localização";
+      }
+    );
+  } else {
+    previsaoTempo.textContent = "Geolocalização não suportada pelo navegador";
+  }
+};
